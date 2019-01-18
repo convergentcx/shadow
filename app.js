ec688c88a1a4026f0653f864d7773f9f6ac3671c
acrdlph <achill@convergent.cx>
2019-01-18 12:16:33 +0100

    update database when user uploads new ipfs data for existing economy

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ mongoose.connect('mongodb+srv://achill:w8BG6xR351pqX6DC@cluster0-xfiey.mongodb.n
         console.log(err);
     })
 
-    
+
 
 // SETUP IPFS AND WEB3 CONNECTION FOR FETCHING USER'S PERSONAL ECONOMY DATA
 
@@ -41,15 +41,15 @@ const factoryAddress = "0xD3f6603fEc53a3175803F5ac99363aA6D3c528Df";
 const ipfs = ipfsApi('ipfs.infura.io', '5001', { protocol: 'https' });
 // need websocket provider for event listening:
 const web3socket = new Web3(new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws"));
-const socketInstance = new web3socket.eth.Contract(factoryAbi, factoryAddress);
+const factoryContractWss = new web3socket.eth.Contract(factoryAbi, factoryAddress);
 // and regular provider for regular contract calls:
 const web3 = new Web3(Web3.givenProvider || "https://rinkeby.infura.io/v3/1169d54f24964d03bbe5264bd501e47f");
-const contract = new web3.eth.Contract(factoryAbi, factoryAddress);
+const factoryContract = new web3.eth.Contract(factoryAbi, factoryAddress);
 
 
 // LISTEN TO FACTORY CONTRACT FOR NEW "CREATED" EVENTS AND STORE NEW ECONOMY'S DATA IN MONGODB
 
-socketInstance.events.Created((err, createdEvent) => {
+factoryContractWss.events.Created((err, createdEvent) => {
     const tokenAddress = createdEvent.returnValues.token_address;
     // get IPFS hash and data
     // instantiate personal economy contract in order to get IPFS hash
@@ -63,7 +63,7 @@ socketInstance.events.Created((err, createdEvent) => {
         });
         ipfs.get(multihash, (err, files) => {
             const dataJson = JSON.parse(files[0].content.toString());
-            const { description, image, name, symbol, services, tags } = dataJson;            
+            const { description, image, name, symbol, services, tags } = dataJson;
             const newEconomy = new Economy({
                 tokenAddress: tokenAddress,
                 ipfsHash: multihash,
@@ -90,16 +90,87 @@ socketInstance.events.Created((err, createdEvent) => {
 })
 
 
-// TBD: LISTEN TO EACH ECONOMY CONTRACT FOR "UPDATED" EVENTS AND STORE NEW ECONOMY'S DATA IN MONGODB
+// LISTEN TO EACH ECONOMY CONTRACT FOR "UPDATED" EVENTS AND STORE NEW ECONOMY'S DATA IN MONGODB
+// NOTE: "UPDATED" EVENT DOES NOT EXIST CURRENTLY - FOR TESTING, THIS IS REPLACED WITH "REQUESTED" EVENT
 // Steps:
-// 1. get array of token addresses from factory contract
-// 2. for each token address instantiate websocket connection, to be able to listen to "updated" event
+// 1. get token addresses from factory contract
+// 2. for each token address instantiate websocket connection, to be able to listen to "updated" event // THIS EVENT HAS TO BE CREATED ON SMART CONTRACTS!
 // 3. on updated event, fetch new ipfs as above, find existing economy in database and save
+
+factoryContract.getPastEvents('Created', { fromBlock: 0, toBlock: "latest" })
+    .then(eventArray => {
+        eventArray.forEach(createdEvent => {
+            const tokenAddress = createdEvent.returnValues.token_address;
+            // instantiate personal economy contract in order to listen to 'Updated' event for each
+            const economyContractWss = new web3socket.eth.Contract(economyAbi, tokenAddress);
+            economyContractWss.events.Requested((err, createdEvent) => { // THIS SHOULD BE CHANGED TO UPDATED, ONCE THAT EVENT EXISTS
+                // get new IPFS hash and data
+                economyContractWss.methods.mhash().call().then(hash => {
+                    // retrieve IPFS data
+                    const multihash = getMultihashFromBytes32({
+                        digest: hash,
+                        hashFunction: 18,
+                        size: 32,
+                    });
+                    ipfs.get(multihash, (err, files) => {
+                        const dataJson = JSON.parse(files[0].content.toString());
+                        const { description, image, name, symbol, services, tags } = dataJson;
+                        Economy.findOne({ tokenAddress: tokenAddress })
+                            .then(economy => {
+                                // If the economy is not yet in the database (for some weird reason), create a new one
+                                if (!economy) {
+                                    const newEconomy = new Economy({
+                                        tokenAddress: tokenAddress,
+                                        ipfsHash: multihash,
+                                        data: {
+                                            name: name,
+                                            symbol: symbol,
+                                            description: description,
+                                            services: services,
+                                            tags: tags
+                                        }
+                                    });
+                                    return newEconomy.save()
+                                        .then(result => {
+                                            console.log('New economy saved.')
+                                        })
+                                        .catch(err => {
+                                            console.log(err);
+                                        });
+                                }
+
+                                economy.ipfsHash = multihash;
+                                economy.data.description = description;
+                                economy.data.services = services;
+                                economy.data.tags = tags;
+
+                                economy.save()
+                                    .then(result => {
+                                        console.log('Existing economy updated.')
+                                        // res.status(201).json({
+                                        //     message: 'Existing economy updated!',
+                                        //     economy: result
+                                        // });
+                                    }).catch(err => {
+                                        console.log(err);
+                                    });
+                            })
+                            .catch(err => {
+                                if (!err.statusCode) {
+                                    err.statusCode = 500;
+                                }
+                                // next(err);
+                            });
+                    });
+                })
+            })
+        })
+    })
 
 
 // GO THROUGH ALL EXISTING ECONOMIES AND MAKE SURE THAT CURRENT DATA IS STORED
 
-contract.getPastEvents('Created', { fromBlock: 0, toBlock: "latest" })
+factoryContract.getPastEvents('Created', { fromBlock: 0, toBlock: "latest" })
     .then(eventArray => {
         eventArray.forEach(createdEvent => {
             const tokenAddress = createdEvent.returnValues.token_address;
@@ -116,10 +187,10 @@ contract.getPastEvents('Created', { fromBlock: 0, toBlock: "latest" })
                     // FOR SOME LOOPS / SOME TOKENS THE IPFS REQUEST FAILS (!) 
                     // THERE SEEMS TO BE A RATE LIMITATION BY IPFS-INFURA
                     // WILL HAVE TO IMPLEMENT POLLING/RETRYING INSTEAD OF JUST PULLING ALL DATA IN ONE GO.
-                    if (!files) { 
+                    if (!files) {
                         // TO SEE WHICH ECONOMIES WHERE NOT FETCHED:
                         // console.log('token: ', tokenAddress, ' ipfsHash: ', multihash)
-                        return 
+                        return
                     }
                     const dataJson = JSON.parse(files[0].content.toString());
                     const { description, image, name, symbol, services, tags } = dataJson;
@@ -159,6 +230,11 @@ contract.getPastEvents('Created', { fromBlock: 0, toBlock: "latest" })
                                     //     message: 'Existing economy updated!',
                                     //     economy: result
                                     // });
+                                }).catch(err => {
+                                    if (!err.statusCode) {
+                                        err.statusCode = 500;
+                                    }
+                                    // next(err);
                                 });
                         })
                         .catch(err => {
